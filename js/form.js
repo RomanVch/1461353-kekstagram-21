7223e2f93d5e7a999e15ea18e3965aea9bf8a462
RomanVch <RomanV92@yandex.ru>
2020-10-13 15:13:00 +0500

    “Scroll1”

--- js/form.js
+++ js/form.js
@@ -14,12 +14,13 @@ const effectLevelPin = window.imgUpload.querySelector(`.effect-level__pin`);
 const effectLevelDepth = window.imgUpload.querySelector(`.effect-level__depth`);
 const effectLevelValue = window.imgUpload.querySelector(`.effect-level__value`);
 let shift = 25;
-
+window.imgUploadEffectLevel.classList.add(`hidden`);
 (() => {
   const funDelEffects = () => {
     window.effectsRadio[0].addEventListener(`change`, () => {
       window.prewiewFoto.className = ``;
       window.imgUploadEffectLevel.classList.add(`hidden`);
+      window.prewiewFoto.style.filter = ``;
     });
   };
   for (let i = 0; i < window.effectsRadio.length; i++) {
